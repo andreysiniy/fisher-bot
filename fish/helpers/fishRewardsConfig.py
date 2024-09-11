c7ffedf18a175a50d47f1770acdf26a788c7e00e
andreysiniy <kakavvmm@gmail.com>
2024-09-11 15:18:57 +0300

    added sub multiplier support

--- fish/helpers/fishRewardsConfig.py
+++ fish/helpers/fishRewardsConfig.py
@@ -3,15 +3,18 @@ import json
 import random
 
 class FishRewards:
-    def __init__(self):
+    def __init__(self, chatterRole):
         self.rewardsFile = 'fishRewards.json'
         with open(self.rewardsFile, 'r') as file:
             self.rewardsJSON = json.load(file)
-
+        self.chatterRole = chatterRole
         self.baseRewards = self.rewardsJSON["rewards"]
         self.cmds = self.rewardsJSON["cmds"]
+        self.multiplier = self.rewardsJSON["base_multiplier"]
+        if self.chatterRole == "sub":
+            self.multiplier = self.rewardsJSON["sub_multiplier"]
         self.chosenReward = self.choose_default_reward()
-
+        print(self.multiplier)
 
     def choose_default_reward(self):
         all_rewards = []
@@ -29,12 +32,16 @@ class FishRewards:
         chosen_reward = random.choices(all_rewards, weights=weights, k=1)[0]
         chosen_type = types[all_rewards.index(chosen_reward)]
 
+
         chosen_reward["type"] = chosen_type
         if chosen_type != "nothing":
             chosen_reward["cmd"] = self.cmds[chosen_type]
         else:
             chosen_reward["cmd"] = ""
-
+        
+        if chosen_reward["type"] == "points":
+            chosen_reward["value"] *= self.multiplier 
+        print(chosen_reward)
         return chosen_reward
 
 #for i in range(2000):
