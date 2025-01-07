4847225a36ca41ef00ae0c5d270f141899b9ac28
andreysiniy <kakavvmm@gmail.com>
2025-01-07 09:51:38 +0300

    fixed !fishrewards cmd

--- fish/helpers/fishRewardsConfig.py
+++ fish/helpers/fishRewardsConfig.py
@@ -1,6 +1,7 @@
 import os.path
 import json
 import random
+import copy
 
 class FishRewards:
     def __init__(self, chatterRole, rewardsFilePath):
@@ -16,10 +17,15 @@ class FishRewards:
         self.chosenReward = self.choose_default_reward()
 
     def get_probabilities(self):
-        total_weight = 0
+
+        total_weight = sum(
+            item.get("weight", 0) 
+            for category in self.baseRewards.values() 
+            for item in category
+        )
+    
         reward_probabilities = {}
-        total_weight = sum(item.get("weight", 0) for category in self.baseRewards.values() for item in category) 
-        print(total_weight)
+
         for category_name, category in self.baseRewards.items():
             if category_name == "nothing":
                 continue  
@@ -29,22 +35,25 @@ class FishRewards:
             for item in category:
                 weight = item.get("weight", 0)
                 probability = round(weight / total_weight, 4) if total_weight > 0 else 0
-                item_data = {"probability": probability}
 
+                item_data = {"probability": probability}
+            
                 if "value" in item:
                     item_data["value"] = item["value"] * self.multiplier
                 elif "seconds" in item:
-                    item_data["seconds"] = item["seconds"]       
+                    item_data["seconds"] = item["seconds"]
+
                 reward_probabilities[category_name].append(item_data)
         return reward_probabilities
        
 
     def choose_default_reward(self):
+        base_rewards_copy = copy.deepcopy(self.baseRewards)
         all_rewards = []
         weights = []
         types = []
 
-        for key, category in self.baseRewards.items():
+        for key, category in base_rewards_copy.items():
             for reward in category:
                 all_rewards.append(reward)
                 weights.append(reward["weight"])
@@ -63,6 +72,4 @@ class FishRewards:
             chosen_reward["value"] *= self.multiplier 
         return chosen_reward
 
-#for i in range(2000):
-#    test = FishRewards()
-#    print(test.chosenReward)
+
