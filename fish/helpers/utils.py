71e8516199914c14b6eccc25c72272b9ef7136f2
andreysiniy <kakavvmm@gmail.com>
2025-06-28 23:13:08 +0300

    debug msgs

--- fish/helpers/utils.py
+++ fish/helpers/utils.py
@@ -138,6 +138,8 @@ def set_command_cooldown(ctx, seconds: int):
 
 def get_command_cooldown(command, ctx, seconds: int):
     key_to_check = (ctx.author.name, ctx.author.id)
+    print(f"Checking cooldown for user: {key_to_check} with seconds: {seconds}")
+    print(f"Command cooldown cache: {command._cooldowns[0]._cache}")
     cooldown_expires = command._cooldowns[0]._cache.get(key_to_check, [0,0])[1] + seconds
     if not cooldown_expires:
         return 0  
