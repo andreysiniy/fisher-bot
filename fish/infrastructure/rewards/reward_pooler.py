2047f381a114b8b159622d4e715cd9f4df19b61c
andreysiniy <kakavvmm@gmail.com>
2025-07-08 22:46:58 +0300

    added RewardPooler implementation
    Introduces the RewardPooler class, implementing the RewardPoolerAbstract interface. Provides a method to select a random reward from a list, assigning type and command based on reward category.

---
+++ fish/infrastructure/rewards/reward_pooler.py
@@ -0,0 +1,27 @@
+from fish.core.interfaces.reward_pooler_abs import RewardPoolerAbstract
+import random
+import copy
+
+class RewardPooler(RewardPoolerAbstract):
+
+    def get_random_reward(self, rewards: list[dict]) -> dict:
+        if not rewards:
+            return {}
+        base_rewards_copy = copy.deepcopy(rewards)
+        all_rewards = []
+        weights = []
+        types = []
+
+        for key, category in base_rewards_copy.items():
+            for reward in category:
+                all_rewards.append(reward)
+                weights.append(reward["weight"])
+                types.append(key)
+        chosen_reward = random.choices(all_rewards, weights=weights, k=1)[0]
+        chosen_type = types[all_rewards.index(chosen_reward)]
+
+        chosen_reward["type"] = chosen_type
+
+        chosen_reward["cmd"] = self.cmds.get(chosen_type, "")
+
+        return chosen_reward
