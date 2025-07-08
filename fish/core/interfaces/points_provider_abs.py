be59f5ed104456f3d835707122ee76d56e3478ad
andreysiniy <kakavvmm@gmail.com>
2025-07-08 20:30:06 +0300

    renamed SEProviderAbstract to PointsProviderAbstract
    Refactored the interface class name for clarity and consistency. This change improves code readability by using a more descriptive class name.

--- fish/core/interfaces/points_provider_abs.py
+++ fish/core/interfaces/points_provider_abs.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod
 from typing import Protocol
 
-class SEProviderAbstract(Protocol):
+class PointsProviderAbstract(Protocol):
 
     @abstractmethod
     async def get_channel_id(self, name: str) -> str:
