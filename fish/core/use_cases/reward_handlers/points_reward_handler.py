e94796c1e64827b673146f793589afa8f3fd1a8b
andreysiniy <kakavvmm@gmail.com>
2025-07-13 05:22:02 +0300

    added PointsRewardHandler for points-based rewards
    Introduces PointsRewardHandler to handle adding or removing points for a user based on the reward configuration. Also supports sending a custom message from the rewards pool if specified.

---
+++ fish/core/use_cases/reward_handlers/points_reward_handler.py
@@ -0,0 +1,36 @@
+from fish.core.use_cases.reward_handlers.base_reward_handler import BaseRewardHandler
+
+class PointsRewardHandler(BaseRewardHandler):
+    def __init__(self, reward: dict, rewards_pool: dict, user_ctx: dict):
+        super().__init__(reward, rewards_pool, user_ctx)
+
+    def handle(self) -> dict:
+        actions = super().handle()
+        
+        points = self.reward.get("points", 0)
+        username = self.user_ctx.get("username", "")
+        delay = self.reward.get("delay", 0)
+
+        if points > 0:
+            actions["actions"].append({
+                "add_points": {
+                    "username": username,
+                    "points": int(points),
+                    "delay": delay
+                }
+            })
+        else:
+            actions["actions"].append({
+                "remove_points": {
+                    "username": username,
+                    "points": int(abs(points)),
+                    "delay": delay
+                }
+            })
+
+        if self.rewards_pool.get("points_message"):
+            actions["actions"].append({
+                "message": self.rewards_pool.get("points_message")
+            })
+
+        return actions
