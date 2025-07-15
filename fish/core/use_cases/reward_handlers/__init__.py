cadde90a7a0641536a256358ce8096b71e046be7
andreysiniy <kakavvmm@gmail.com>
2025-07-15 07:26:05 +0300

    added NothingRewardHandler and CustomRewardHandler to __init__ file

--- fish/core/use_cases/reward_handlers/__init__.py
+++ fish/core/use_cases/reward_handlers/__init__.py
@@ -5,6 +5,8 @@ from .points_reward_handler import PointsRewardHandler
 from .robbery_reward_handler import RobberyRewardHandler
 from .russian_roulette_reward_handler import RussianRouletteRewardHandler
 from .timeout_reward_handler import TimeoutRewardHandler
+from .nothing_reward_handler import NothingRewardHandler
+from .custom_reward_handler import CustomRewardHandler
 
 __all__ = [
     "BaseRewardHandler",
@@ -13,5 +15,7 @@ __all__ = [
     "PointsRewardHandler",
     "RobberyRewardHandler",
     "RussianRouletteRewardHandler",
-    "TimeoutRewardHandler"
+    "TimeoutRewardHandler",
+    "NothingRewardHandler",
+    "CustomRewardHandler"
 ]
